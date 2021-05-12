7f871249ea5efc53340eaa90d702eddb299cb158
mscott44 <moriahscott27@gmail.com>
2021-05-11 17:32:45 -0700

    fixed mobile?

--- src/components/MenuItems.js
+++ src/components/MenuItems.js
@@ -1,7 +1,7 @@
-export const MenuItems = [
-  {
-    title: "Coding",
-    path: "/projects",
-    cName: "dropdown-link",
-  },
-];
+// export const MenuItems = [
+//   {
+//     title: "Coding",
+//     path: "/projects",
+//     cName: "dropdown-link",
+//   },
+// ];
