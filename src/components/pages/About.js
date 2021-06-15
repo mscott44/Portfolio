e05a73ad5a24c5ba5f9d02c0823223fc0f0756e4
mscott44 <moriahscott27@gmail.com>
2021-06-14 23:51:13 -0700

    edited about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -8,17 +8,17 @@ function About() {
       <div className="about-text">
         <p>
           As you may have gathered from my homepage, my name is Moriah and
-          welcome to my Center of Innovation. I am a Junior computer science
+          welcome to my Center of Innovation. I am a Senior computer science
           major, studio arts minor at Loyola Marymount University. Since I was a
           kid I always found new ways to be creative and demonstrate my
-          abilities. I have a passion for design and tech, and through frontend
-          development, I am able to continue being creative, and express myself
-          through technology. The satisfaction of seeing designs come to life
-          almost instantly, is unbeatable. There is no better feeling then to
-          look at a finished product and know that it all started with one line
-          of code and an image in mind. As of now, I only have three projects to
-          show, one of them being this portfolio site. However, as I progress I
-          hope to have tons more!
+          abilities. I have a passion for design and tech, through frontend
+          development and UI/UX design, I am able to continue being creative, as
+          well as express myself through technology. The satisfaction of seeing
+          designs come to life almost instantly, is unbeatable. There is no
+          better feeling then to look at a finished product and know that it all
+          started with one line of code and an image in mind. As of now, I only
+          have three projects to show, one of them being this portfolio site.
+          However, as I progress I hope to have tons more!
         </p>
       </div>
       <p className="about-adios"> LET'S GET INNOVATIVE! </p>
