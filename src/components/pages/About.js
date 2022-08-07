a71356be42c19f8a118c0e575e6085c11ba46f51
mscott44 <moriahscott27@gmail.com>
2022-08-06 18:00:09 -0700

    making design decisions

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,26 +2,30 @@ import React from "react";
 import "../../App.css";
 function About() {
   return (
-    <div className="about__container">
+    <div>
+      {" "}
       <div className="about-image-wrapper">
         <div className="about-image"></div>
       </div>
-
-      <div className="about-text">
-        <p className="text">
-          <div className="hey">
-            <strong>hey there!</strong>
-          </div>
-          <br></br>
-          I'm from San Jose, California and I've recently graduated with a
-          bachelors in Computer Science! I am currently pursuing an opportunity
-          in UI/UX design or something closely related (design marketing or
-          front end engining) and really excited to take that first step in my
-          career as a creative in a highly technical environment. I have
-          experience with Figma, react, and javascript which are showcased in
-          the projects on my portfolio
-        </p>
-      </div>
+      <ul>
+        <div>
+          <section className="projects-wrapper">
+            {/* <p className="text"> */}
+            <div className="hey">
+              <strong>hey there!</strong>
+            </div>
+            <br></br>
+            I'm from San Jose, California and I've recently graduated with a
+            bachelors in Computer Science! I am currently pursuing an
+            opportunity in UI/UX design or something closely related (design
+            marketing or front end engineering) and really excited to take that
+            first step in my career as a creative in a highly technical
+            environment. I have experience with Figma, react, and javascript
+            which are showcased in the projects on my portfolio
+            {/* </p> */}
+          </section>
+        </div>
+      </ul>
     </div>
   );
 }
