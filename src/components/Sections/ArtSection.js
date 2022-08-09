94b00ec6e797bdf6b399efd53927c8f9c27d2aee
mscott44 <moriahscott27@gmail.com>
2022-08-08 19:54:23 -0700

    added more art

--- src/components/Sections/ArtSection.js
+++ src/components/Sections/ArtSection.js
@@ -7,8 +7,10 @@ function ArtSection() {
   return (
     <div className="main-container">
       <h1> My Art Gallery </h1>
-      <p style={{ fontSize: "20px" }}>
-        Photoshop, Illustrator, Procreate, & Hand Drawings{" "}
+      <p style={{ fontSize: "20px" }}> I specialize in creativity!</p>
+      <p style={{ fontSize: "10px" }}>
+        {" "}
+        Photoshop, Illustrator, Procreate, Hand Drawings, Photography, Corel
       </p>
       <div className="buttons-container">
         <div className="main-btns">
--- src/components/tileData.js
+++ src/components/tileData.js
@@ -4,9 +4,60 @@ const tileData = [
     title: "Lacerate Movie Poster",
     // featured: true,
   },
+
+  {
+    img: "artImages/piano.jpeg",
+    title: "Lacerate Movie Poster",
+    // featured: true,
+  },
+  {
+    img: "artImages/kobbe.png",
+    title: "Lacerate Movie Poster",
+    // featured: true,
+  },
+  {
+    img: "artImages/colorism.jpeg",
+    title: "Lacerate Movie Poster",
+    // featured: true,
+  },
+  {
+    img: "artImages/maeve.jpeg",
+    title: "Lacerate Movie Poster",
+    // featured: true,
+  },
   {
     img: "artImages/pinkthumb.png",
     title: "Pink Thumb",
+    // featured: true,
+  },
+  {
+    img: "artImages/afwd.jpeg",
+    title: "Lacerate Movie Poster",
+    // featured: true,
+  },
+  {
+    img: "artImages/muse.jpeg",
+    title: "Lacerate Movie Poster",
+    // featured: true,
+  },
+  {
+    img: "artImages/Pink_Panther.jpeg",
+    title: "Lacerate Movie Poster",
+    // featured: true,
+  },
+  {
+    img: "artImages/crosscol.png",
+    title: "Lacerate Movie Poster",
+    // featured: true,
+  },
+  {
+    img: "artImages/dp.png",
+    title: "Lacerate Movie Poster",
+    // featured: true,
+  },
+  {
+    img: "artImages/fosters.jpeg",
+    title: "Lacerate Movie Poster",
     // featured: true,
   },
   {
@@ -20,6 +71,11 @@ const tileData = [
     title: "Moriah's Prom Issue",
     // featured: true,
   },
+  {
+    img: "artImages/kelly.jpeg",
+    title: "Lacerate Movie Poster",
+    // featured: true,
+  },
 
   {
     img: "artImages/santaclarafinal.png",
@@ -28,6 +84,11 @@ const tileData = [
   {
     img: "artImages/vogue3.jpg",
     title: "Vogue Cover",
+  },
+  {
+    img: "artImages/advocate.png",
+    title: "Lacerate Movie Poster",
+    // featured: true,
   },
   {
     img: "artImages/uow.png",
